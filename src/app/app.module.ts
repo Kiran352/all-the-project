3437745e50a233d63aa03314a9aa06a618839c54
kiran <kiran.pc99@gmail.com>
2020-02-20 11:46:51 +0530

    FIrstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { Routes,RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
+
+const appRoutes:Routes=
+[
+{path:'',component:HomeComponent},
+
+{path:'courses',component:CoursesComponent},
+{path:'contactus',component:ContactusComponent}
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
